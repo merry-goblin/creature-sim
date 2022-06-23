92bfe5e4d6f307930a8a3fa1afd9d1432e2b8a77
MerryGoblin <keller.alexandre+0@gmail.com>
2022-06-24 01:24:11 +0200

    refactoring

--- Assets/Scripts/Simulation/World/AbstractWorld.cs
+++ Assets/Scripts/Simulation/World/AbstractWorld.cs
@@ -5,11 +5,15 @@ public abstract class AbstractWorld
 {
     public List<ISubject> subjectList;
     public List<IElement> elementList;
+    public List<ISubject> subjectListToLoad;
+    public List<IElement> elementListToLoad;
 
     public AbstractWorld()
     {
         this.subjectList = new List<ISubject>();
         this.elementList = new List<IElement>();
+        this.subjectListToLoad = new List<ISubject>();
+        this.elementListToLoad = new List<IElement>();
     }
 
     /**
@@ -17,24 +21,28 @@ public abstract class AbstractWorld
      */
     public virtual void Load()
     {
-        for (int i = 0, nb = this.subjectList.Count; i < nb; i++)
+        for (int i = 0, nb = this.subjectListToLoad.Count; i < nb; i++)
         {
-            this.subjectList[i].Load();
+            this.subjectListToLoad[i].Load();
+            this.subjectList.Add(this.subjectListToLoad[i]);
         }
+        this.subjectListToLoad.Clear();
 
-        for (int i = 0, nb = this.elementList.Count; i < nb; i++)
+        for (int i = 0, nb = this.elementListToLoad.Count; i < nb; i++)
         {
-            this.elementList[i].Load();
+            this.elementListToLoad[i].Load();
+            this.elementList.Add(this.elementListToLoad[i]);
         }
+        this.elementListToLoad.Clear();
     }
 
     protected void AddSubject(ISubject subject)
     {
-        this.subjectList.Add(subject);
+        this.subjectListToLoad.Add(subject);
     }
     protected void AddElement(IElement element)
     {
-        this.elementList.Add(element);
+        this.elementListToLoad.Add(element);
     }
 
     public virtual void Update()
