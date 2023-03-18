accf1589c06d9d52083a110154ec8dc212a9f249
MerryGoblin <keller.alexandre+0@gmail.com>
2023-03-18 01:00:44 +0100

    a main menu added and I m starting tp work on backpropagation

---
+++ Assets/Scripts/Learning/GradientDescent/GradientDescentAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections.Generic;
+using FeedForwardNeuralNetwork;
+
+namespace Learning.GradientDescent
+{
+    public class GradientDescentAlgorithm
+    {
+        protected NeuralNetwork neuralNetwork;
+
+        public GradientDescentAlgorithm(NeuralNetwork neuralNetwork)
+        {
+            this.neuralNetwork = neuralNetwork;
+        }
+
+        public void Train(List<List<float>> inputValueSet, List<List<float>> expectedOutputValueSet)
+        {
+            //  One loop is done on set of values but it should be more than that: this is a work in progress
+            for (int i = 0, nb = inputValueSet.Count; i < nb; i++)
+            {
+                List<float> inputValues = inputValueSet[i];
+                List<float> expectedOutputValues = expectedOutputValueSet[i];
+
+                this.FeedForward(inputValues);
+                this.Backpropagation(expectedOutputValues);
+            }
+        }
+
+        protected void FeedForward(List<float> inputValues)
+        {
+            this.neuralNetwork.ApplyInputValues(inputValues);
+            this.neuralNetwork.CalculateOutput();
+        }
+
+        protected void Backpropagation(List<float> expectedOutputValues)
+        {
+            //  @todo
+        }
+    }
+}
